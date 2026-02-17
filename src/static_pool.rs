af807d49f43af4348c89871cb540d2169f7ce0fe
Frédéric Vauchelles <frederic.vauchelles@outlook.com>
2026-02-17 11:35:19 +0100

    bevy_string_intern no_std use bevy_platform for sync primitives in strpool

--- src/static_pool.rs
+++ src/static_pool.rs
@@ -1,5 +1,5 @@
 use super::Pool;
-use spin::Mutex;
+use bevy_platform::sync::{Mutex, PoisonError};
 
 #[derive(Debug)]
 pub struct PoolCell<const P: usize> {
@@ -17,13 +17,13 @@ impl<const P: usize> PoolCell<P> {
 
     /// Swaps the inner pool with a new one
     pub fn swap(&self, pool: Pool<P>) {
-        let mut guard = self.pool.lock();
+        let mut guard = self.pool.lock().unwrap_or_else(PoisonError::into_inner);
         *guard = Some(pool);
     }
 
     /// Retrieves the inner pool
     pub fn pool(&self) -> Pool<P> {
-        let mut guard = self.pool.lock();
+        let mut guard = self.pool.lock().unwrap_or_else(PoisonError::into_inner);
 
         if guard.is_none() {
             *guard = Some(Pool::new());
